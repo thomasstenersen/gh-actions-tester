cc57e9ce068d4ba65847a040821916735e50b859
Thomas Stenersen <thomas.stenersen@easee.com>
2022-09-02 08:01:18 +0200

    Add passing test case

--- test/src/example_test.c
+++ test/src/example_test.c
@@ -7,3 +7,11 @@ Test(example_test, failure_test)
     int expected = 5;
     cr_expect_eq(actual, expected);
 }
+
+
+Test(example_test, success_test)
+{
+    int actual = 5;
+    int expected = 5;
+    cr_expect_eq(actual, expected);
+}
